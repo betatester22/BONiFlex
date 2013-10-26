288da979bfa7cf3dc161e2611ba250ef5f208aa9
betatester22 <betatester22@me.com>
2013-10-27 00:12:30 +0200

    Header.php hinzugefügt

---
+++ _/components/php/header.php
@@ -0,0 +1,9 @@
+<div class="content row">
+	<div class="col-lg-12">
+		<header class="clearfix">
+			<section id="branding">
+				<a href="index.php"><img src="images/Screenshot 2013-10-20 14.37.51.png" alt="Screen"></a>
+			</section><!-- branding -->
+		</header><!-- clearfix -->
+	</div><!-- column -->
+</div><!-- content -->
