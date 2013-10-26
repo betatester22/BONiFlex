f8136fa44fe6e637961155d8cf2ca4dccf33149a
betatester22 <betatester22@me.com>
2013-10-26 23:54:50 +0200

    Fonts hinzugefügt

--- index.php
+++ index.php
@@ -5,16 +5,29 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="_/css/boniflex-fonts.css">
   </head>
   <body id="home">
 
     <section class="container">
       <div class="content row">
         <section class="main col col-lg-8">
-
+          <h2>Headline</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section><!-- main -->
         <section class="sidebar col col-lg-4">
-
+          <h2>Headline</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </section><!-- sidebar -->
       </div><!-- content -->
     </section><!-- container -->
--- office.php
+++ office.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="_/css/bootstrap.css" rel="stylesheet" media="screen">
     <link href="_/css/mystyles.css" rel="stylesheet" media="screen">
+    <link rel="stylesheet" href="_/css/boniflex-fonts.css">
   </head>
   <body id="office">
 
